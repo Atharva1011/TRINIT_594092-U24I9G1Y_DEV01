8aefa637bc2ba251256f20d7633881614f2f54db
Atharva Deshmukh <atharva.deshmukh10@gmail.com>
2023-02-12 07:02:17 +0530

    Embedding admin dashboard

--- src/router/ngo.js
+++ src/router/ngo.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const url = require("url");
-const multer = require('multer');
+const multer = require("multer");
 
 const mongoose = require("mongoose");
 const Ngo = require("../models/ngo");
@@ -9,103 +9,99 @@ const Activity = require("../models/activity");
 
 // const {registerNGO} = require("../controllers/ngo.js");
 
-const fileStorageEngine =  multer.diskStorage({ 
-  destination: (req,file,cb)=>{
-     cb(null, './uploads/ngoProfile')
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/ngoProfile");
   },
-  filename: (req,file,cb)=>{
-      cb(null, Date.now()+ '--' + file.originalname)
-   }
-})
-const upload = multer({ storage : fileStorageEngine });
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "--" + file.originalname);
+  },
+});
+const upload = multer({ storage: fileStorageEngine });
 
-router.get("/ngo/register", async(req, res)=>{
+router.get("/ngo/register", async (req, res) => {
   res.render("ngo_reg");
 });
 
-router.get("/ngo/login", async(req, res)=>{
+router.get("/ngo/login", async (req, res) => {
   res.render("ngo_login");
 });
 
-
-router.post("/ngo/register", async(req,res)=>{
+router.post("/ngo/register", async (req, res) => {
   const ngo = await Ngo.create(req.body);
   console.log(req.body);
-  if(!ngo){
-      console.log("Error creating student");
-      return;
-  };
+  if (!ngo) {
+    console.log("Error creating student");
+    return;
+  }
   console.log(ngo);
-  
+
   res.redirect("/");
 });
 
-router.post("/ngo/login", async(req, res) => {
-    const {email, password}= req.body;
+router.post("/ngo/login", async (req, res) => {
+  const { email, password } = req.body;
 
-    //checking if ngo has given email and password both
-    console.log(email, password);
-    if(!email || !password){
-       
-      return res.status(400).json({ msg :"Please enter email and password" });
-    }
+  //checking if ngo has given email and password both
+  console.log(email, password);
+  if (!email || !password) {
+    return res.status(400).json({ msg: "Please enter email and password" });
+  }
 
-    const ngo= await Ngo.findOne({email: email}).select("+password");
-    if(!ngo){
-      return res.status(400).json({ msg :"NGO does not exist" });
-    }
+  const ngo = await Ngo.findOne({ email: email }).select("+password");
+  if (!ngo) {
+    return res.status(400).json({ msg: "NGO does not exist" });
+  }
 
-    console.log("ngo",ngo);
-    let isPasswordMatched=false;
-    if(password === ngo.password){
-      isPasswordMatched=true;
-    }
-    else{
-        return res.status(400).json({ msg :"Invalid Credentials" });
-    }
-    res.redirect("/" + `ngo/${ngo._id}`);
+  console.log("ngo", ngo);
+  let isPasswordMatched = false;
+  if (password === ngo.password) {
+    isPasswordMatched = true;
+  } else {
+    return res.status(400).json({ msg: "Invalid Credentials" });
+  }
+  res.redirect("/" + `ngo?ngo_id=${ngo._id}`);
 });
 
-
-router.post("/ngo/newActivity/:ngo_id", async(req, res) => {
+router.post("/ngo/newActivity/:ngo_id", async (req, res) => {
   const activity = await Activity.create(req.body);
 
-  const ngo_id= req.params.ngo_id;
-  const ngo= await Ngo.findById(ngo_id);
+  const ngo_id = req.params.ngo_id;
+  const ngo = await Ngo.findById(ngo_id);
 
   console.log(req.body);
-  if(!activity){
-      console.log("Error creating student");
-      return;
-  };
+  if (!activity) {
+    console.log("Error creating student");
+    return;
+  }
   console.log(activity);
-  
+
   res.render("ngo_reg");
 });
 
-router.get("/ngo/:id", async(req, res) => {
-  const ngo = await Ngo.findById(req.params.id);
-  if(!ngo){
-    return res.status(404).json({ msg :"NGO not found" });
+router.get("/ngo", async (req, res) => {
+  const ngo = await Ngo.findById(req.query.ngo_id);
+  if (!ngo) {
+    return res.status(404).json({ msg: "NGO not found" });
   }
-  
-  res.status(200).json({ 
-    success:true,
-    ngo
-})
+
+  // res.status(200).json({
+  //   success: true,
+  //   ngo,
+  // });
+  res.render("ngo_dash", { name: ngo.name, category: "NGO" });
 });
 
-router.get("/activity/:id", async(req, res) => {
+router.get("/activity/:id", async (req, res) => {
   const activity = await Activity.findById(req.params.id);
-  if(!activity){
-    return res.status(404).json({ msg :"No registered drive" });
+  if (!activity) {
+    return res.status(404).json({ msg: "No registered drive" });
   }
-  
-  res.status(200).json({ 
-    success:true,
-    activity
-})
-});
 
+  res.status(200).json({
+    success: true,
+    activity,
+  });
+});
 
 module.exports = router;
--- src/router/phila.js
+++ src/router/phila.js
@@ -3,56 +3,63 @@ const router = new express.Router();
 const mongoose = require("mongoose");
 const Phila = require("../models/phila");
 const Activity = require("../models/activity");
+const Ngo = require("../models/ngo");
 
-router.post("/phila/register", async(req, res) => {
+router.post("/phila/register", async (req, res) => {
   const phila = await Phila.create(req.body);
   console.log(req.body);
-  if(!phila){
-      console.log("Error creating philanthrophist");
-      return;
-  };
+  if (!phila) {
+    console.log("Error creating philanthrophist");
+    return;
+  }
   console.log(phila);
-  
+
   res.redirect("/");
 });
 
-router.get("/phila/login", (req, res) => {
-  res.render("phila_login");
+router.post("/phila/login", async (req, res) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+  //checking if ngo has given email and password both
+  console.log(email, password);
+  if (!email || !password) {
+    return res.status(400).json({ msg: "Please enter email and password" });
+  }
+
+  const phila = await Phila.findOne({ email: email }).select("+password");
+  if (!phila) {
+    return res.status(400).json({ msg: "Philanthrophist does not exist" });
+  }
+
+  console.log("phila", phila);
+  let isPasswordMatched = false;
+  if (password === phila.password) {
+    isPasswordMatched = true;
+  } else {
+    return res.status(400).json({ msg: "Invalid Credentials" });
+  }
+
+  res.redirect("/phila" + `?phila_id=${phila._id}`);
 });
 
-router.get("/phila/", (req, res) => {
-  res.render("phila_login");
-});
+router.get("/phila", async (req, res) => {
+  const phila = await Phila.findById(req.query.phila_id);
+  if (!phila) {
+    return res.status(404).json({ msg: "Philanthropist not found" });
+  }
 
-
-router.post("/phila/login", async(req, res) => {
-    const {email, password}= req.body;
-    console.log(req.body);
-    //checking if ngo has given email and password both
-    console.log(email, password);
-    if(!email || !password){
-      return res.status(400).json({ msg :"Please enter email and password" });
-    }
-
-    const phila= await Phila.findOne({email: email}).select("+password");
-    if(!phila){
-      res.status(400).json({ msg :"Philanthrophist does not exist" });
-    }
-
-    console.log("phila",phila);
-    let isPasswordMatched=false;
-    if(password === phila.password){
-      isPasswordMatched=true;
-    }
-    else{
-        return res.status(400).json({ msg :"Invalid Credentials" });
-    }
-
-    res.redirect("/");
+  res.render("phila_dash", {
+    name: `${phila.firstName} ${phila.lastName}`,
+    category: "Philanthropist",
+  });
 });
 
 router.get("/phila/register", (req, res) => {
   res.render("phila_reg");
+});
+
+router.get("/phila/login", (req, res) => {
+  res.render("phila_login");
 });
 
 module.exports = router;
